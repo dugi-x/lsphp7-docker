4b7da49bdd05850997e9441d35d42d5c88749af7
Dugi <dugi2000@gmail.com>
2020-01-27 09:32:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,27 @@
 _LSPHP=${LSPHP:-73}
 if [ -f "/usr/local/lsws/lsphp${_LSPHP}/bin/lsphp" ]; then
     ln -sf /usr/local/lsws/lsphp${_LSPHP}/bin/lsphp /usr/local/lsws/fcgi-bin/lsphp
-    echo "lsphp  version set to: ${_LSPHP}"
+    echo "lsphp   switch to version: ${_LSPHP}"
 fi
 if [ -f "/usr/local/lsws/lsphp${_LSPHP}/bin/php" ]; then
     ln -sf /usr/local/lsws/lsphp${_LSPHP}/bin/php /usr/bin/php
-    echo "cliphp version set to: ${_LSPHP}"
+    echo "php-cli switch to version: ${_LSPHP}"
 fi
 
-exec "$@"
+
+$@
+
+trap "exit" INT
+while true; do
+
+    status=$(/usr/local/lsws/bin/lswsctrl status)
+    if [ "$status" != "$(cat lswsctrl.status 2>/dev/null)" ]; then
+            echo $status | tee lswsctrl.status
+    fi
+
+    if ! grep 'litespeed is running with PID *' <<< $status > /dev/null ; then
+        break
+    fi
+
+    sleep 60
+done
